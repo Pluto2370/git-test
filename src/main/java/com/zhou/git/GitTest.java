37db3c7f8e5e444944c337233c3eec238c890b3e
plout <1025924298@qq.com>
2022-06-17 21:51:50 +0800

    idea third commit

--- src/main/java/com/zhou/git/GitTest.java
+++ src/main/java/com/zhou/git/GitTest.java
@@ -6,5 +6,6 @@ package com.zhou.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello ideaGit!");
+        System.out.println("hot-fix ideaGit!");
     }
 }
