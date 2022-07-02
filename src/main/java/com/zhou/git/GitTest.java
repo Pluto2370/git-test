d4e3feff4e98251f43e1174a2be74d8c2a866272
Pluto2370 <107242818+Pluto2370@users.noreply.github.com>
2022-07-02 09:59:32 +0800

    GitHub commit

--- src/main/java/com/zhou/git/GitTest.java
+++ src/main/java/com/zhou/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello ideaGit!!!!");
         System.out.println("hot-fix ideaGit!");
+        System.out.println("GitHub commit");
     }
 }
